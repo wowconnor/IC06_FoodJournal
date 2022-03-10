575b998f2d2b38b68b28aa688ac820ff013e6f43
mymac <connormarks14@gmail.com>
2022-03-10 12:47:18 -0800

    commit!

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +7,7 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
         String name;
         int calories, cookingTemp, type, carbs;
-
+        File file = new File("food.dat");
     }
+
 }
